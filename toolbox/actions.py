0bd3309c195c73d9abbdb419514327624d8f7aa8
Jony-Jas <jonyjas@gmail.com>
2021-10-10 12:43:45 +0530

    action.py

--- toolbox/actions.py
+++ toolbox/actions.py
@@ -1,23 +1,28 @@
 import json
+import os
+
 
 class Actions():
     def audio():
-        f = open ('data.json', "r")
+        f = open('data.json', "r")
         data = json.loads(f.read())
         data = data["sample"]
         f.close()
         path = data
         return path
+
     def input():
-        f = open ('data.json', "r")
+        f = open('data.json', "r")
         data = json.loads(f.read())
         data = data["data"]
         f.close()
         return data
+
     def save():
-        f = open ('data.json', "r")
+        f = open('data.json', "r")
         data = json.loads(f.read())
         id = data["id"]
         f.close()
-        fpath = "C:/Users/jonyj/Documents/Project/Ongoing/Hackathon/Voice Cloning/cloned_audios/"+id
+        directory = os.path.join(os.getcwd(), "../cloned_audios")
+        fpath = os.path.abspath(directory) +"/"+id
         return fpath
--- toolbox/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-f = open ('data.json', "r")
-data = json.loads(f.read())
-for i in data['table']:
-    print(i)
-f.close()
